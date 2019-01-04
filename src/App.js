cf360af8e1f694dd87176d4abd19da6f1b495b85
Xin Shen <xin.shen@target-energysolutions.com>
2019-01-04 14:06:59 +0800

    :sparkles: [feat] add gift code

--- src/App.js
+++ src/App.js
@@ -1,21 +1,60 @@
-import React, { Component } from 'react';
+import React, {Component, Fragment} from 'react'
+import {remove, sample} from 'lodash-es'
 import logo from './logo.svg';
 import './App.css';
+import immer from 'immer'
+const person = [
+  '张昊','曾娅萱','申鑫','邓晓峰','胡飞','张可人','龙涛','胡如康'
+]
+export default class Home extends Component {
+  state = {
+    prizePool: person,
+    result: person.map(i => ({
+      name: i,
+      gift: null
+    }))
+  }
+  getGift = name => () => {
+    const {prizePool} = this.state
+    const gift = sample(prizePool.filter(i => i !== name))
+    this.setState(
+      immer(state => {
+        remove(state.prizePool, i => i === gift)
+        state.result.find(i => i.name === name).gift = gift
+      })
+    )
 
-class App extends Component {
+  }
+  reset = () => {
+    this.setState({
+      prizePool: person,
+      result: person.map(i => ({
+        name: i,
+        gift: null
+      }))
+    })
+  }
   render() {
+    const {result} = this.state
     return (
       <div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h2>Welcome to silvia</h2>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <div className="App-intro">
+        {
+          result.map(i => (<div key={i.name}>
+            <span>{i.name}</span>
+            {i.gift ? <span>奖品: {i.gift}</span>
+            : <button onClick={this.getGift(i.name)}>抽奖</button>}
+          </div>))
+        }
+        <button onClick={this.reset}>重置结果</button>
       </div>
-    );
-  }
-}
+      </div>
 
-export default App;
+    )
+  }
+
+}
